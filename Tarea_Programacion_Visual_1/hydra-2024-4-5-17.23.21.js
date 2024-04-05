cf20d41d7a2d467519ad9931802c54cfdcb758df
Javierjmz11 <141684062+Javierjmz11@users.noreply.github.com>
2024-04-05 17:29:34 -0600

    Add files via upload

---
+++ Tarea_Programacion_Visual_1/hydra-2024-4-5-17.23.21.js
@@ -0,0 +1,10 @@
+
+ // Type some code on a new line (such as "osc().out()"), and press CTRL+shift+enter
+
+// Taller de Creacion Audio Visual
+// autor: Yair Jair Javier Montalvo Jimenez
+// Universidad Autonoma Metropolitana
+// 05 abril 2024
+
+voronoi(5).scrollY(0.05,0.09).colorama(1.9).out()
+
